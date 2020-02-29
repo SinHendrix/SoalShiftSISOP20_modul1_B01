bf99aaf86a06f8e1918635a897b6e733bae9a015
SinHendrix <56729915+SinHendrix@users.noreply.github.com>
2020-02-29 14:46:45 +0000

    Create soal2d.sh

---
+++ soal2/soal2d.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+upperAlphabet=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
+lowAlphabet=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+name=$1
+enkrip=${name%%.*}
+shift=$(ls -l $nama | cut -d ' ' -f '8' | head -c 2)
+enkname=$(echo $enkrip | tr "${upperAlphabet:${shift}:26}" "${upperAlphabet:0:26}" | tr "${lowAlphabet:${shift}:26}" >
+mv $name $enkname.txt
